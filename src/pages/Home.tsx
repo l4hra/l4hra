2b1ff238040d0c3b83d15dc2ae43aeb1e60ba15c
Lahra <lahras.nuness@gmail.com>
2024-09-11 18:01:21 -0300

    iniciando portifolio

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      {/* centraliza div */}
+      <div className="h-screen w-screen bg-orange-300 flex justify-center items-center gap-5 flex-col">
+        <p className="text-5xl">Apresentação</p>
+        <p className="">Frase de efeito/resuminho</p>
+        <p className="">continuação/conclusão</p>
+        <p className="">continuação/conclusão</p>
+      </div>
+    </>
+  );
+}
